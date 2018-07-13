5b7b198eb8551335815b0002acb64c96fcdded9c
Bruno De Lucia <25172812+brunodelucia82@users.noreply.github.com>
2018-07-13 17:41:19 +0200

    feat: Implement page reload on click of the div with class 'restart'

--- js/app.js
+++ js/app.js
@@ -86,3 +86,9 @@ function addMatch(card) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ 
+let restart = document.querySelector(".restart");
+
+restart.addEventListener("click", function() {
+	location.reload();
+})
